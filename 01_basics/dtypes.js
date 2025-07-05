dbdbac8508ba9f05727c7bbc3808317242971f48
Kunal Dagar <79902312+KUNAL-DAGAR-2002@users.noreply.github.com>
2025-07-05 17:06:11 +0000

    one js added

---
+++ 01_basics/dtypes.js
@@ -0,0 +1,27 @@
+// Data types
+
+// -> Primitive
+// -> Non Primitive
+
+// Based on storage and access of data
+
+// Primitive (call by value : a copy is return)
+// 1. String
+// 2. Number
+// 3. Boolean,
+// 4. Null,
+// 5. undefined,
+// 6. symbol -> to make something unique
+// 7. Bigint
+
+// Non Primitive (call by reference : original is sent)
+// 1. Array
+// 2. Objects
+// 3. Functions
+
+
+// JS is dynamically typed, dtype is decided at runtime
+
+
+// STACK MEMORY -> PRIMITIVE
+// HEAP -> Non Primitive
---
+++ 01_basics/one.js
@@ -0,0 +1,38 @@
+// Conversions
+
+// let age = 33
+
+// let age1 = "33"
+// age2 = Number(age1)
+
+// let age0 = "33abc"
+// age3 = Number(age0)
+// console.table([typeof age, typeof age2, typeof age3, age2, age3,null,undefined, true])
+
+// Operations
+// let value = "hello"
+// let val2 = "sir"
+
+// console.log(value + val2);
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2) // prefered type first string in this case
+// console.log((1+2+"2"));
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// // num1 = num2 = num3 = 2 + 2 Not recommended
+
+// COMPARISON OF DATA TYPES
+
+// STRICT CHECK => === check val with datatype
+
+console.log("2" === 2);
+console.log("2" == 2); // Avoid
+
+
+
+
